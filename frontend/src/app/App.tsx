b13931b3ab5b747446956d265686d945e2d267a7
leoneves <eleonardoneves@gmail.com>
2021-01-07 21:59:44 -0300

    initialize frontend project and config eslint and prettier

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Login from './components/Login';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/app/components/Login/index.tsx
@@ -0,0 +1,23 @@
+import React, { useRef } from 'react';
+
+const Login: React.FC = () => {
+  const usernameLogin = useRef<HTMLInputElement>(null);
+
+  const loginSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredUsername = usernameLogin.current!.value;
+  };
+
+  return (
+    <form onSubmit={loginSubmitHandler}>
+      <div>
+        <label htmlFor={'username'}>User</label>
+        <input type={'text'} id={'username'} ref={usernameLogin} />
+      </div>
+      <button type={'submit'}>Login</button>
+    </form>
+  );
+};
+
+export default Login;
