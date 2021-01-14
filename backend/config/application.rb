7007fda56668fe8476262ca8a0f370a8edf50ccf
leoneves <eleonardoneves@gmail.com>
2021-01-13 23:53:06 -0300

    adding staging for rack CORS

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -34,7 +34,7 @@ module Backend
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    if ENV['RAILS_ENV'] == 'development'
+    if %w[development staging].include? ENV['RAILS_ENV']
       config.middleware.insert_before 0, Rack::Cors do
         allow do
           origins '*'
