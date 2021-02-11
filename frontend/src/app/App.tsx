2bfa19b84a11fbfeba7d47c9cc42c0afcbe535c1
leoneves <eleonardoneves@gmail.com>
2021-02-11 15:39:28 -0300

    adding playground for development

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent, lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ContainerApp } from './App.styles';
 import Loading from './components/Loading/Loading';
+import Playground from './Playground/Playground';
 
 const Login = lazy(() => import('./components/Login/Login'));
 const Home = lazy(() => import('./containers/Home/Home'));
@@ -11,7 +12,8 @@ const App: FunctionComponent = (): JSX.Element => {
     <Suspense fallback={<Loading />}>
       <ContainerApp>
         <Switch>
-          <Route path="/home" component={Home} />
+          {process.env.NODE_ENV === 'development' && <Route path="/playground" exact component={Playground} />}
+          <Route path="/home" exact component={Home} />
           <Route path="/" component={Login} />
         </Switch>
       </ContainerApp>
---
+++ frontend/src/app/Playground/Playground.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+
+const Playground: FunctionComponent = (): JSX.Element => {
+  return <p>Playground</p>;
+};
+
+export default Playground;
